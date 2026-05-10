a805ec58a6e3a383cddc503466c68981398fdb00
Kornpitak Kannika <kornpitukgunk@gmail.com>
2026-05-10 16:17:19 +0700

    Refactor code structure for improved readability and maintainability

--- apps/web/.next/server/server-reference-manifest.js
+++ apps/web/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"hgNxdBmzxfjitr/ZeXMmjpJbEhgCoKChi6uKZ9fa5CQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"T12QQP8f+rUPGdUD7TUC176XaNKmHVYMtTLYUPvEt20=\"}"
